f99ded2dc3ac7c893137cdb2ea00c33f4184d347
unknown <lancelaudencia0@gmail.com>
2026-09-12 15:36:27 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.photogallery.mobile',
+  appName: 'Photo_Gallery',
+  webDir: 'dist'
+};
+
+export default config;
