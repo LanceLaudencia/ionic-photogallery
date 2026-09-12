cec301155b951164c09b2cd051859e0a77e60d6b
unknown <lancelaudencia0@gmail.com>
2026-09-12 16:29:02 +0800

    building

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,18 +7,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD95kuyOhTudTq0GBSuaapfaooJLj4PTso",
-  authDomain: "ionic-photo-gallery-e4fda.firebaseapp.com",
-  databaseURL: "https://ionic-photo-gallery-e4fda-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ionic-photo-gallery-e4fda",
-  storageBucket: "ionic-photo-gallery-e4fda.firebasestorage.app",
-  messagingSenderId: "524082105567",
-  appId: "1:524082105567:web:38cfd385371cc45012ed6a",
-  measurementId: "G-ZWSLDKJHD4"
+  apiKey: "AIzaSyDgWl1sQ9vCwLw9gML4kmG6PPD68lXp6FM",
+  authDomain: "ionic-photogallery-crud.firebaseapp.com",
+  databaseURL: "https://ionic-photogallery-crud-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ionic-photogallery-crud",
+  storageBucket: "ionic-photogallery-crud.firebasestorage.app",
+  messagingSenderId: "103833441079",
+  appId: "1:103833441079:web:60752351098d1fb7bbc0b2",
+  measurementId: "G-KE4K820RZ8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export const database = getDatabase(app);
